9555dfa22ce591d1e1c59d3b942aea6d8fb43e7f
Qiwen Hu <huqiwen0313@gmail.com>
2020-12-11 21:50:48 -0500

    code: add RNA pipeline scripts

---
+++ scripts/RNA.sample.QC.R
@@ -0,0 +1,132 @@
+# this script is used to generate sample-level QC object and statistics
+# Qiwen Hu - 2020
+
+library(pagoda2)
+library(dropestr)
+library(Seurat)
+library("DropletUtils")
+suppressMessages(library(org.Hs.eg.db))
+library(snarePip)
+
+## functions
+GetGenesetFraction_overall <- function (count.matrix, genes){
+  umi.counts <- sort(Matrix::colSums(count.matrix), decreasing = T)
+  presented.mit.genes <- intersect(genes, rownames(count.matrix))
+  genes.frac <- sum(Matrix::colSums(count.matrix[presented.mit.genes, 
+                                                 names(umi.counts)]))/sum(umi.counts)
+  return(genes.frac)
+}
+####
+
+# parameters
+args = commandArgs(trailingOnly=TRUE)
+path <- args[1]
+sampleID <- args[2]
+scrublet_path <- args[3]
+
+# QC cutoffs
+min.cell.size <- 200
+min.ngenes <- 200
+max.ngenes <- 7500
+min.ncells <- 500
+
+objdir <- file.path(path, "obj")
+count <- readRDS(file.path(objdir, paste(sampleID, "sample_matrix.rds", sep=".")))
+# filtering
+sizePcell <- colSums(count)
+# filtering by cell size
+fcells <- names(sizePcell[sizePcell>min.cell.size])
+count <- count[, colnames(count) %in% fcells]
+
+
+# doublet score
+if(dim(count)[2] > min.ncells){
+  scrublescore <- snarePip:::GetScrubletScores(count, min.molecules.per.gene=1, method="scrublet",
+                                    pythonPath=scrublet_path)
+  doublet.detect <- snarePip:::GetScrubletScores(count, min.molecules.per.gene=10, method="doubletDetection",
+                                      pythonPath=scrublet_path)
+  if(!file.exists(file.path(path, "doublets"))){
+    dir.create(file.path(path, "doublets"))
+  }
+  doublet.path <- file.path(path, "doublets")
+  saveRDS(list(scrublet=scrublescore, doubletDetection=doublet.detect), 
+          file.path(doublet.path, paste(sampleID, "doublet.rds", sep=".")))
+}
+
+
+# pagoda object
+p2 <- snarePip:::p2proc(count, n.cores=20, min.cells.per.gene=5, min.cell.size=200, n.odgenes=2e3, 
+                         get.largevis=FALSE, make.geneknn=FALSE, perplexity=50, log.scale = TRUE, 
+                         batch = NULL, nPcs = 30, k = 30, get.tsne = TRUE)
+if(dim(count)[2] > min.cell.size){
+  p2[["doublet"]] <- list(scrublet=scrublescore, doubletDetection=doublet.detect)
+}
+saveRDS(p2, file.path(objdir, paste(sampleID, "p2.rds", sep=".")))
+
+# create p2 App
+if(dim(count)[2] > min.cell.size){
+  ids <- unlist(lapply(mget(colnames(p2$counts), org.Hs.egALIAS2EG, ifnotfound=NA), function(x) x[1]))
+  rids <- names(ids); names(rids) <- ids
+  go.env <- list2env(eapply(org.Hs.egGO2ALLEGS,function(x) as.character(na.omit(rids[x]))))
+  p2$makeGeneKnnGraph(n.cores=5) 
+  p2web <- snarePip:::GetPagodaWebApp(p2, p2$clusters$PCA$multilevel, additional.metadata=list(), 
+                verbose=T, go.sets=NULL, go.env=go.env, test.pathways=TRUE)
+  # save object
+  p2web$serializeToStaticFast(paste(objdir,"/",sampleID, ".p2App.bin",sep=""), verbose=T)
+}
+
+# generate QC statistics
+mit_genes <- c("MT-ND1",
+               "MT-ND2",
+               "MT-ND3",
+               "MT-ND4",
+               "MT-ND4L",
+               "MT-ND5",
+               "MT-ND6",
+               "MT-CYB",
+               "MT-CO1",
+               "MT-CO2",
+               "MT-CO3",
+               "MT-ATP6",
+               "MT-ATP8")
+
+aligned_UMIs_per_cell <- Matrix::colSums(count)
+median_UMIs_per_cell <- median(aligned_UMIs_per_cell)
+number_of_cells_after_filtering <- ncol(count)
+
+if(number_of_cells_after_filtering>1){
+  percent_read_mapping_to_mitochrondrial<- round(GetGenesetFraction_overall(count, mit_genes), 4)
+} else{
+  percent_read_mapping_to_mitochrondrial<-"Too few cells to calculate"
+}
+
+if(number_of_cells_after_filtering>1){
+  median_fraction_read_in_mitochondrial <- round(median(GetGenesetFraction(count, mit_genes)), 4)
+}else{
+  median_fraction_read_in_mitochondrial <-"Too few cells to calculate"
+}
+
+if(number_of_cells_after_filtering < min.ncells){
+  Sample_quality <- "Not_Pass"
+} else{Sample_quality <- "Pass"}
+
+qc.stat <- c(median_UMIs_per_cell, median_fraction_read_in_mitochondrial,
+             percent_read_mapping_to_mitochrondrial, number_of_cells_after_filtering,
+             Sample_quality, paste(objdir,"/",sampleID, ".p2App.bin",sep=""))
+
+measurement <- c("median_UMIs_per_cell", "median_fraction_read_in_mitochondrial",
+                 "percent_read_mapping_to_mitochrondrial", "number_of_cells_after_filtering",
+                 "Sample_quality", "p2_webApp_address")
+
+
+qc.stat <- data.frame(measurement=measurement, stat=qc.stat)
+qc.dir <- file.path(path, "QCs")
+if(!file.exists(qc.dir)){
+  dir.create(qc.dir)
+}
+write.table(qc.stat, quote = FALSE, col.names = FALSE, row.names = FALSE, sep="\t", 
+            file.path(qc.dir, paste(sampleID, "qc.txt", sep=".")))
+
+# output tenX format
+tenX.dir <- file.path(path, "matrix_tenX")
+write10xCounts(tenX.dir, count, overwrite=T)
---
+++ scripts/distr.sample.R
@@ -0,0 +1,160 @@
+# This script is used to merge samples from different libraries based on ATAC/RNA folder structure
+# Usage: Rscript merge.samples.R sample_dir TRUE/FALSE RNA/ATAC TRUE/FALSE snare_2 path_to_sample_table
+# Qiwen Hu - 2020
+
+library(SnapATAC)
+library(pagoda2)
+library(snarePip)
+
+##
+args = commandArgs(trailingOnly=TRUE)
+path <- args[1]
+# delete tmp file or not
+tmpFile <- args[2]
+# RNA or ATAC
+type <- args[3]
+# if link sample ID to libID
+linkLIbID <- args[4]
+# assay type
+assay <- args[5]
+# path to sample table
+link.path <- args[6]
+samtool.path <- args[7]
+
+tmp.path <- file.path(path, "tmp")
+if(type=="ATAC"){
+  directory <- file.path(tmp.path, "snap")
+} else{
+  directory <- file.path(tmp.path, "pagoda_RData")
+}
+
+# directory of link table
+if(linkLIbID){
+  link.table <- read.table(link.path, sep="\t", header=TRUE)
+}
+
+files <- unique(gsub("[.].*|-SP.*|_N.*", "", dir(directory)))
+for(i in 1:length(files)){
+  tissue <- unique(snarePip:::getTissue(link.table, files[i]))
+  filePath <- file.path(path, assay, tissue, "samples")
+  if(!dir.exists(file.path(path, assay, tissue))){
+    dir.create(file.path(path, assay))
+    dir.create(file.path(path, assay, tissue))
+    dir.create(file.path(path, assay, tissue, "samples"))
+  }
+  
+  # sample snap files
+  if(type=="ATAC"){
+    snapFiles <- system(paste("ls", file.path(directory, paste(files[i], "*.snap", sep="")), sep=" "), 
+                        intern=TRUE)
+    # extract fragment file paths
+    frag.path <- file.path(tmp.path, "fragments")
+    fragmentFiles <- system(paste("ls", file.path(frag.path, paste(files[i], "*.sort.bed", sep="")), sep=" "), 
+                            intern=TRUE)
+  
+    # create folders
+    system(paste("mkdir", file.path(filePath, files[i]), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output"), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output", "snap"), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output", "objects"), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output", "fragments"), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output", "macs2"), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output", "bam"), sep=" "),
+           intern=TRUE)
+    
+    # merge fragment files 
+    system2(command="cat", args=c(paste(fragmentFiles, collapse = ' '),
+                                  ">", paste(file.path(filePath, files[i], "Sample_output", "fragments", files[i]), 
+                                             "fragments.bed", sep=".")))
+    # extract bam file paths
+    bam.path <- file.path(tmp.path, "bam")
+    bamFiles <- system(paste("ls", file.path(bam.path, paste(files[i], "*.bam", sep="")), sep=" "), 
+                            intern=TRUE)
+    bamFiles <- bamFiles[grep(paste(files[i], ".*rmsk.bam", sep=""), bamFiles, perl=TRUE)]
+    bamFiles <- paste(bamFiles, collapse=" ")
+   
+    # merge bamFiles
+    outputbamFile <- paste(file.path(filePath, files[i], "Sample_output", "bam", files[i]), "bam", sep=".")
+    system(paste(samtool.path, "merge", outputbamFile, bamFiles, "-@ 15", sep=" "),
+           intern=TRUE)
+    
+    # create initial merged p2 object
+    p2 <- Pagoda2$new()
+    p2[["fragmentsPaths"]] <- paste(file.path(filePath, files[i], "Sample_output", "fragments", files[i]), 
+                                    "fragments.bed", sep=".")
+    saveRDS(p2, paste(file.path(filePath, files[i], "Sample_output", "objects", files[i]), "p2.obj.rds", sep="."))
+  } else{
+    pagodaFiles <- system(paste("ls", file.path(directory, paste(files[i], "*filtered.RData", sep="")), sep=" "), 
+                          intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i]), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output"), sep=" "),
+           intern=TRUE)
+    system(paste("mkdir", file.path(filePath, files[i], "Sample_output", "obj"), sep=" "),
+           intern=TRUE)
+    cMatrix <- lapply(pagodaFiles, function(r){
+      t <- load(r)
+      get(t)})
+    mergedMatrix <- snarePip:::merge.sparse(cMatrix)
+    saveRDS(mergedMatrix, paste(file.path(filePath, files[i], "Sample_output", "obj", files[i]), "sample_matrix.rds", sep="."))
+  }
+  
+  # Split experiment data
+  expDir <- file.path(file.path(filePath, files[i]), "Experiment_output")
+  system(paste("mkdir", expDir, sep=" "), intern=TRUE)
+  if(type=="ATAC"){
+    folders <- c("bam", "fastqFiles", "fragments", "macs", "objects",
+                 "pmats", "QCs", "reports", "snap")
+  } else{
+    folders <- c("alignment", "dropest_out", "FastQC", "pagoda_RData", 
+                 "QCs", "seurat_obj", "tagged", "reports")
+  }
+  
+  for(j in 1:length(folders)){
+    system(paste("mkdir", file.path(expDir, folders[j]), sep=" "), intern=TRUE)
+    if(linkLIbID){
+      #folder.files <- dir(file.path(tmp.path, folders[j]))
+      folder.files <- system(paste("ls", file.path(tmp.path, folders[j], paste(files[i], "*", sep="")), sep=" "),
+                             intern=TRUE)
+      folder.files <- gsub(".*/", "", folder.files)
+      
+      lapply(1:length(folder.files), function(r){
+        # get full name experiment ID
+        sampleid <- gsub("_S\\d+.*", "_S", folder.files[r])
+        libID <- snarePip:::getLibID(link.table, sampleid)
+        if(length(libID) > 0){
+          surfix <- gsub(".*_S\\d+.", "", folder.files[r])
+          sample.files <- file.path(tmp.path, paste(folders[j], folder.files[r], sep="/"))
+          target.files <- file.path(expDir, 
+                                    paste(folders[j], paste(libID, surfix, sep="."), sep="/"))
+          system(paste("cp", sample.files, target.files, sep=" "))
+          #if(folders[j] == "QCs"){
+          #  target.files <- file.path(expDir, 
+          #                            paste(folders[j], folder.files[r], sep="/"))
+          #  system(paste("cp", sample.files, target.files, sep=" "))
+          #}
+        } else{
+          sample.files <- file.path(tmp.path, paste(folders[j], folder.files[r], sep="/"))
+          target.files <- file.path(expDir, 
+                                    paste(folders[j], folder.files[r], sep="/"))
+          system(paste("cp", sample.files, target.files, sep=" "))
+        }
+      })} else{
+      system(paste("cp", paste(file.path(tmp.path, folders[j]), "/", files[i], "*", sep=""),
+                   file.path(expDir, folders[j]), sep=" "), intern=TRUE)
+    }
+  }
+}
+
+# move log files out
+system(paste("mv", file.path(tmp.path, "logs/*"), file.path(path, "logs"), sep=" "), intern=TRUE)
+if(tmpFile){
+  system(paste("rm -rf", file.path(path, "tmp"), sep=" "), intern=TRUE)
+}
+system(paste("rm", file.path(path, "*filtered.bam"), sep=" "), intern=TRUE)
